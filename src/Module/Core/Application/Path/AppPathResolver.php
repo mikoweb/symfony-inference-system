623fe396a834503e4ffb6a5495ff4d3ded37b2f8
Rafał Mikołajun <root@rmweb.pl>
2024-01-02 03:46:08 +0100

    add AppPathResolver

---
+++ src/Module/Core/Application/Path/AppPathResolver.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Module\Core\Application\Path;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use function Symfony\Component\String\u;
+
+final readonly class AppPathResolver
+{
+    public function __construct(
+        private ParameterBagInterface $parameterBag
+    ) {}
+
+    public function getAppPath(?string $path = null): string
+    {
+        return $this->concatPath($this->parameterBag->get('kernel.project_dir'), $path);
+    }
+
+    public function getResourcesPath(?string $path = null): string
+    {
+        return $this->getAppPath($this->concatPath('resources', $path));
+    }
+
+    public function getPredefinedDataPath(?string $path = null): string
+    {
+        return $this->getResourcesPath($this->concatPath('predefined_data', $path));
+    }
+
+    public function concatPath(string $basePath, ?string $path): string
+    {
+        if (empty($path)) {
+            return $basePath;
+        }
+
+        if (u($path)->startsWith('/')) {
+            $path = u($path)->slice(1)->toString();
+        }
+
+        return "$basePath/$path";
+    }
+}
