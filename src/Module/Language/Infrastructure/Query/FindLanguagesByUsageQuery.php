1eadacdbefb4b4096381886df969e50ad50ad25c
Rafał Mikołajun <root@rmweb.pl>
2024-01-05 00:16:39 +0100

    add FindLanguagesByUsageQuery

---
+++ src/Module/Language/Infrastructure/Query/FindLanguagesByUsageQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Module\Language\Infrastructure\Query;
+
+use App\Module\Language\Domain\ProgrammingLanguage\LanguageData;
+use App\Module\Language\Domain\ProgrammingLanguage\LanguageUsageList;
+use App\Module\Language\Domain\ProgrammingLanguage\ProgrammingLanguagesDataset;
+
+class FindLanguagesByUsageQuery
+{
+    /**
+     * @param ProgrammingLanguagesDataset|LanguageData[] $dataset
+     * @param LanguageUsageList $usage
+     * @return ProgrammingLanguagesDataset|LanguageData[]
+     */
+    public function findByUsage(
+        ProgrammingLanguagesDataset $dataset,
+        LanguageUsageList $usage
+    ): ProgrammingLanguagesDataset
+    {
+        return $dataset->filter(function (LanguageData $data) use($usage) {
+            $match = false;
+
+            foreach ($usage as $enum) {
+                if (in_array($enum, $data->usage->toArray())) {
+                    $match = true;
+                    break;
+                }
+            }
+
+            return $match;
+        });
+    }
+}
--- src/Module/LanguageChoice/Domain/LanguageFilter.php
+++ src/Module/LanguageChoice/Domain/LanguageFilter.php
@@ -2,17 +2,14 @@
 
 namespace App\Module\LanguageChoice\Domain;
 
-use App\Module\Language\Domain\ProgrammingLanguage\LanguageUsageEnum;
+use App\Module\Language\Domain\ProgrammingLanguage\LanguageUsageList;
 use App\Module\LanguageChoice\Domain\Fuzzy\PerformanceLevelEnum;
 use App\Module\LanguageChoice\Domain\Fuzzy\PopularityLevelEnum;
 
 final readonly class LanguageFilter
 {
     public function __construct(
-        /**
-         * @var LanguageUsageEnum[]|null
-         */
-        public ?array $usage = null,
+        public ?LanguageUsageList $usage = null,
         public ?PerformanceLevelEnum $minimumPerformanceLevel = null,
         public ?PopularityLevelEnum $minimumPopularityLevel = null,
     ) {}
