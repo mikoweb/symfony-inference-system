1a5e10788e088b3da1e8c86d470afa43c8863f70
Rafał Mikołajun <root@rmweb.pl>
2024-01-17 20:26:39 +0100

    load api url form environment

--- frontend/src/app/module/core/application/config/config.ts
+++ frontend/src/app/module/core/application/config/config.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export default class Config {
   public getApiBaseUrl(): string {
-    return this.getEnvData().apiBaseUrl ?? '/';
+    return this.getEnvData().apiBaseUrl ?? environment.apiBaseUrl;
   }
 
   public getEnvData(): any {
